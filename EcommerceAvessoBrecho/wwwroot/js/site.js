7bcc0582ab2252788259808157ee3fd3a5d7886c
Renan Emanoel Bonassa <renanemanoel07@gmail.com>
2021-05-01 16:41:31 -0300

    Manutenção Front-End
    Ajustado alinhamento subtotal (Carrinho);
    Criado Modal para Camisas (Detalhe Produto);
    Ajustado para aparecer preço em promoção (Pesquisa Produto);
    Ajustado para aparecer a marca do produto (Pesquisa Produto).

--- EcommerceAvessoBrecho/wwwroot/js/site.js
+++ EcommerceAvessoBrecho/wwwroot/js/site.js
@@ -1,4 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+function AtualizaCaminho() {
+    document.getElementById("lbl_Caminho").value = caminho;
+    alert("Passou aqui");
+}
+
 // Write your JavaScript code.
