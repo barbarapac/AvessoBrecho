0e5e897cba44773e2de68f7fa55fd12897146604
barbarapac <babiantunesp@gmail.com>
2021-05-06 23:09:37 -0300

    Bloqueio envio de email.
    - Bloqueado método apenas par anão ultrapassar limite disponibilizado pelo serviço.

--- EcommerceAvessoBrecho/Repositories/SendEmailRepository.cs
+++ EcommerceAvessoBrecho/Repositories/SendEmailRepository.cs
@@ -41,7 +41,7 @@ namespace EcommerceAvessoBrecho.Repositories.IRepository
                     smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
                     smtp.UseDefaultCredentials = false; 
                     smtp.Credentials = new NetworkCredential(credenciais.Email, credenciais.Pass);
-                    //smtp.Send(mail);
+                    //smtp.Send(mail); PARA ENVIAR EMAIL SÓ DESCOMENTAR AQUI.
                 }
                 catch (Exception ex){ }
             }
