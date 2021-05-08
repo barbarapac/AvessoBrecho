3752979d9757bb7ac204014f5f3f24b06cc389d9
Renan Emanoel Bonassa <renanemanoel07@gmail.com>
2021-05-08 14:34:34 -0300

    Front - End (Fix)
    Ajustado padding inputs do resumo do pedido;
    Ajustado padding títulos do resumo do pedido;
    Ajustado padding inputs do cadastro de cliente;
    Ajustado padding títulos do cadastro de cliente;
    Desenvolvido .js para alterar propriedade do navbar e caminho (Não deu certo)
    Criado .js para deixar botão de continuar na tela de cadastro desabilitado até que todos os campos obrigatórios sejam preenchidos;
    Criado modal Calça;
    Criado modal Calçado;

--- EcommerceAvessoBrecho/wwwroot/js/site.js
+++ EcommerceAvessoBrecho/wwwroot/js/site.js
@@ -1,9 +1,21 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function AtualizaCaminho() {
-    document.getElementById("lbl_Caminho").value = caminho;
-    alert("Passou aqui");
+var roupa = window.document.getElementById('a_Roupas');
+var caminho = window.document.getElementById('lbl_Caminho');
+
+roupa.addEventListener('mouseup', selecionaRoupa);
+
+function selecionaRoupa() {
+
+    //alert('Aquiiii');
+    roupa.style.fontSize = '20px';
+    roupa.style.textDecoration = 'underline';
+    caminho.innerText = 'Roupas';
 }
+
+$(document).ready(function () {
+    selecionaRoupa;
+});
 
 // Write your JavaScript code.
